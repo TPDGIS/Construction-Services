687a441ccb9194ca5be7bb3e5887a3b2103f534e
AMcGinleyTPD <TPDGIS@tpdinc.com>
2026-02-20 11:27:25 -0500

    Update Push

---
+++ cdn/25/widgets/password_widget/dist/setting/translations/default.ts
@@ -0,0 +1,5 @@
+export default {
+  _widgetLabel: 'Password Widget',
+  targetPageId: 'Target Page ID',
+  targetPageIdHint: 'Enter the ID of the page to navigate to when the correct password is entered.'
+};
