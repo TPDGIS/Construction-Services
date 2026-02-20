04f959400dc8909d68c8a9aa4fef9732f7d60341
AMcGinleyTPD <TPDGIS@tpdinc.com>
2026-02-20 11:25:51 -0500

    Delete

--- cdn/24/widgets/password_widget/dist/setting/translations/default.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  _widgetLabel: 'Password Widget',
-  targetPageId: 'Target Page ID',
-  targetPageIdHint: 'Enter the ID of the page to navigate to when the correct password is entered.'
-};
